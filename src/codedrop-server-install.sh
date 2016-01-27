31c1090fe562b8c81dab9a825bc46ee0ed402d9e
Dean Massecar <microhextech@gmail.com>
2016-01-27 09:49:51 -0330

    added more to install script, also added MUN git link in the readme

--- src/codedrop-server-install.sh
+++ src/codedrop-server-install.sh
@@ -1,6 +1,7 @@
-sudo sh
+#run as root/sudo
 apt-get update
 apt-get install -y mosh mc tmux htop iotop iftop nano # not required, but useful utilities
 apt-get install -y openjdk-7-jdk openjdk-7-jre openjdk-7-jre-headless
 / openjdk-7-jre-lib openjdk-7-jre-zero openjdk-7-doc python python-pip tomcat7
 wget -qO- https://get.docker.com/ | sh # installs docker
+usermod -aG docker dean #replace with the application user account that auto runs
