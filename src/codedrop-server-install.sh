446f02b008b6077024bfbb1d11aa57821550e9dd
Dean Massecar <microhextech@gmail.com>
2016-01-27 09:38:52 -0330

    Added the first bit of the VM install script.

---
+++ src/codedrop-server-install.sh
@@ -0,0 +1,6 @@
+sudo sh
+apt-get update
+apt-get install -y mosh mc tmux htop iotop iftop nano # not required, but useful utilities
+apt-get install -y openjdk-7-jdk openjdk-7-jre openjdk-7-jre-headless
+/ openjdk-7-jre-lib openjdk-7-jre-zero openjdk-7-doc python python-pip tomcat7
+wget -qO- https://get.docker.com/ | sh # installs docker
