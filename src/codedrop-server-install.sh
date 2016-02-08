5aa10ca2e4a0376d20ca47e44d472d69c28fadd7
Dean Massecar <dean.massecar@gmail.com>
2016-02-08 14:14:41 -0330

    exported wireframes as PDF for easier viewing

--- src/codedrop-server-install.sh
+++ src/codedrop-server-install.sh
@@ -5,3 +5,4 @@ apt-get install -y openjdk-7-jdk openjdk-7-jre openjdk-7-jre-headless
 / openjdk-7-jre-lib openjdk-7-jre-zero openjdk-7-doc python python-pip tomcat7
 wget -qO- https://get.docker.com/ | sh # installs docker
 usermod -aG docker dean #replace with the application user account that auto runs
+reboot #probably need to reboot here to get the docker daemon running and have all permissions set appropriately
