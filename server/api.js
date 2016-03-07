cd24967066a307bfeaad5d06653c66075889d120
pmsc73 <philipmstc@gmail.com>
2016-03-07 15:16:19 -0330

    Changed double quotes to single quotes, whoops

--- server/api.js
+++ server/api.js
@@ -25,48 +25,48 @@ router.get('/get_courses', function(req, res) {
 });
 
 router.get('/get_assignment_list', function(req, res) {
-  res.send("Sample assignment list");
+  res.send('Sample assignment list');
 });
 
 router.get('/get_directory', function(req, res) {
-  res.send("Sample directory");
+  res.send('Sample directory');
 });
 
 router.get('/get_file', function(req, res) {
-  res.send("Sample file");
+  res.send('Sample file');
 });
 
 router.get('/save_file', function(req, res) {
-  res.send("Sample save success/fail");
+  res.send('Sample save success/fail');
 });
 
 router.get('/download_file', function(req, res) {
-  res.send("Sample download success/fail statement");
+  res.send('Sample download success/fail statement');
 });
 
 router.get('/save_directory', function(req, res) {
-  res.send("Sample assignment list");
+  res.send('Sample assignment list');
 });
 
 
 // INSTRUCTOR SPECIFIC
 
 router.get('/save_marks', function(req, res) {
-  res.send("Sample marks saved success/fail");
+  res.send('Sample marks saved success/fail');
 });
 
 // ADMIN SPECIFIC
 
 router.get('/save_courses', function(req, res) {
-  res.send("Sample courses saved success/fail");
+  res.send('Sample courses saved success/fail');
 });
 
 router.get('/get_users', function(req, res) {
-  res.send("Sample users list");
+  res.send('Sample users list');
 });
 
 router.get('/save_users', function(req, res) {
-  res.send("Sample users saved success/fail");
+  res.send('Sample users saved success/fail');
 });
 
 module.exports = router;
