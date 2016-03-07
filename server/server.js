8d24b9a712a7f482283688c64c9af7c7763459de
Tim <tim@CodeDrop>
2016-03-07 14:06:37 +0000

    added pg

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ var express = require('express');
  
 var server = express();
 server.use(express.static(__dirname + '/public'));
+
+server.get('/theotherside', function (req, res) {
+  res.send('Hello from the other side!');
+});
  
 var port = 8081;
 server.listen(port, function() {
