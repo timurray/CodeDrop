bafdf50f9f81bf2dc2a7b2a57b59751e40d8f675
pmsc73 <philipmstc@gmail.com>
2016-03-07 15:12:46 -0330

    Updated api module to respond to all methods

--- server/api.js
+++ server/api.js
@@ -6,9 +6,67 @@ router.get('/', function(req, res) {
   res.sendFile('public/api.html', {root: __dirname });
 });
 
+/****************************************************
+ * API METHOD ACCESS ENTRY POINT FACTORY GENERATION
+ * CLASS DESCRIPTION DIAGRAM FACTORY for 
+ * 	CodeDrop! made and licsenced by:
+ * TEAM CSSTCS4770TPAPT2016WSAMUN Winter 2016
+ ****************************************************
+ *
+ * Methods names are specified in the first part of get
+ * example: router.get('[method_name]', function())
+ *
+ ****************************************************/
+ 
+// GENERAL API METHODS
+
 router.get('/get_courses', function(req, res) {
   res.sendFile('public/test.json', {root: __dirname });
 });
 
-module.exports = router;
+router.get('/get_assignment_list', function(req, res) {
+  res.send("Sample assignment list");
+});
 
+router.get('/get_directory', function(req, res) {
+  res.send("Sample directory");
+});
+
+router.get('/get_file', function(req, res) {
+  res.send("Sample file");
+});
+
+router.get('/save_file', function(req, res) {
+  res.send("Sample save success/fail");
+});
+
+router.get('/download_file', function(req, res) {
+  res.send("Sample download success/fail statement");
+});
+
+router.get('/save_directory', function(req, res) {
+  res.send("Sample assignment list");
+});
+
+
+// INSTRUCTOR SPECIFIC
+
+router.get('/save_marks', function(req, res) {
+  res.send("Sample marks saved success/fail");
+});
+
+// ADMIN SPECIFIC
+
+router.get('/save_courses', function(req, res) {
+  res.send("Sample courses saved success/fail");
+});
+
+router.get('/get_users', function(req, res) {
+  res.send("Sample users list");
+});
+
+router.get('/save_users', function(req, res) {
+  res.send("Sample users saved success/fail");
+});
+
+module.exports = router;
