0c774edf65613bd01e5101157a893e07e855937b
Tim <tim@CodeDrop>
2016-03-18 18:03:05 +0000

    added a main.js, fixed the html pages to use google sign in scripts more and also link to the routes instead of static pages

---
+++ server/main.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var sqlite = require('sqlite3').verbose();
+
+var db = new sqlite.Database("codedrop.db");
+
+router.get('/', function(req, res) {
+  res.sendFile('public/index.html', {root: __dirname });
+});
+
+router.get('/login', function(req, res) {
+  res.sendFile('public/login.html', {root: __dirname });
+});
+
+router.get('/contact', function(req, res) {
+  res.sendFile('public/contact.html', {root: __dirname });
+});
+
+//router.get('
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var api = require('./api');
+var main = require('./main');
 
 var server = express();
 server.use(express.static(__dirname + '/public'));
 
 server.use('/api', api);
+server.use('/', main);
 
 var port = 8081;
 server.listen(port, function() {
