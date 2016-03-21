420a3511e2839c32eba436c6cd69db93d590f921
Shane Butt 200621753 <b25stb@mun.ca>
2016-03-21 09:42:12 -0230

    msg

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var api = require('./api');
 var main = require('./main');
+var compile = require('./compilerequest');
 
 var server = express();
 server.use(express.static(__dirname + '/public'));
 
 server.use('/api', api);
 server.use('/', main);
+server.use('/compile',compile);
 
 var port = 8081;
 server.listen(port, function() {
