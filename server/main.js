55b41da42e8a4530b523593c307ff5e86a31203a
pmsc73 <philipmstc@gmail.com>
2016-03-21 09:26:14 -0230

    made student submissions page

--- server/main.js
+++ server/main.js
@@ -25,10 +25,6 @@ router.get('/soln', function(req, res) {
 	res.sendFile('public/stucode.html', {root: __dirname });
 });
 
-router.get('/mark', function(req, res) {
-	res.sendFile('public/asignedit.html', {root: __dirname });
-});
-
 
 function getAssignments(user_id) {
 	return "SELECT W.* FROM work W, register R WHERE W.course_id = R.course_id AND R.user_id = " + user_id;
@@ -48,6 +44,17 @@ var workId = 0;
 var courses = [];
 var assigns = [];
 var register = [];
+
+router.get('/submissions', function(req, res) {
+	resBody.concat("<ul>");
+	db.serialize( function() {
+		db.each("SELECT S.name FROM users S, register R WHERE R.user_id = S.user_id AND R.role = 0 AND R.course_id = " + req.query.id, function(err, row) {
+			resBody = resBody.concat("<li>"+JSON.stringify(row)+"</li>");
+		}
+	}
+	res.send(resBody);
+	resBody ="";
+});
 
 router.get('/courses', function(req, res) {
     db.serialize(function() {
@@ -76,7 +83,7 @@ router.get('/courses', function(req, res) {
 				
 							resBody = resBody.concat("<li>"+assigns[j].title);
 							if(register[r].role == 1) {
-								resBody = resBody.concat("&nbsp;&nbsp;<a href='edit?id="+assigns[j].work_id+"'>[Edit]</a>&nbsp;&nbsp;<a href='mark?id="+assigns[j].work_id+"'>Student Submissions</a>");
+								resBody = resBody.concat("&nbsp;&nbsp;<a href='edit?id="+assigns[j].work_id+"'>[Edit]</a>&nbsp;&nbsp;<a href='submissions?id="+courses[i].course_id+"'>Student Submissions</a>");
 							}
 							else if(register[r].role == 0) {
 								resBody = resBody.concat("&nbsp;&nbsp;<a href='soln?id="+assigns[j].work_id+"'>View Your Solution</a>");
