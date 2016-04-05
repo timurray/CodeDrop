11880c1076780679c3db845e46413faa7846c29b
Shane <b25stb@mun.ca>
2016-04-05 19:01:57 +0000

    Changed wrong thing fixed it tho

--- db_create.sql
+++ db_create.sql
@@ -69,11 +69,13 @@ CREATE TABLE tests (
 );
 
 -- Table: file_storage
-CREATE TABLE file_storage ( 
-	fs_id INTEGER NOT NULL PRIMARY KEY UNIQUE, 
-	capacity INTEGER, 
-	name VARCHAR,
-	user_id INTEGER REFERENCES users (user_id) ON DELETE CASCADE
+CREATE TABLE file_storage (
+    fs_id    INTEGER NOT NULL
+                     PRIMARY KEY
+                     UNIQUE,
+    capacity INTEGER,
+    user_id  INTEGER REFERENCES users (user_id) ON DELETE CASCADE,
+    work_id  INT     REFERENCES work (work_id) ON DELETE CASCADE
 );
 
 -- Table: sessions
